f2881fdc24028d434449c1b54812fc19f32cb8a8
李昭君 <lizhao_jun@aliyun.com>
2018-07-22 23:18:11 +0800

    no message

---
+++ 语言类/JAVA/代码注释/test.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package test1;
+
+/**
+ * @author lizhao_jun
+ *
+ */
+public class Go {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+	}
+
+}
