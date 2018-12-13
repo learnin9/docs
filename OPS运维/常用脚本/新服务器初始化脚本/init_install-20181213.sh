db272bd47a738be0ec4d7ada99e0ffa80755047e
运维那些破事 <lizhao_jun@aliyun.com>
2018-12-13 18:08:56 +0800

    添加了内核优化的参数

--- OPS运维/常用脚本/新服务器初始化脚本/init_install-20181213.sh
+++ OPS运维/常用脚本/新服务器初始化脚本/init_install-20181213.sh
@@ -22,6 +22,8 @@ echo      "正在安装开发者工具,请稍等一会儿....."
 echo "----------------------------------------------"
 sudo yum -y groupinstall "Development Tools"
 
+sudo echo  > /etc/chrony.conf
+sudo cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 sudo cat > /etc/chrony.conf  << _EEOF
 server 0.centos.pool.ntp.org iburst
 server 1.centos.pool.ntp.org iburst
@@ -38,5 +40,85 @@ echo "-------------------------------------------------------------"
 echo "正在启动chronyd时间同步(等同于NTP,CentOS7版本已全线变更)服务"
 echo "-------------------------------------------------------------"
 sudo service chronyd restart && sudo chkconfig chronyd on
+
+
+sudo echo  > /etc/security/limits.conf
+sudo cat > /etc/security/limits.conf  << _BEOF
+root soft nofile 65535
+root hard nofile 65535
+_BEOF
+
+
+echo "-------------------------"
+echo "   正在进行内核优化   "
+echo "-------------------------"
+echo " "
+echo "######################################################"
+echo " 注意:"
+echo "     1. 该脚本在通用内核优化方案的基础上进行修改利用 " 
+echo "     2. 如有需要,可以根据自己的需求进行修改          "  
+echo "######################################################"
+echo " "
+echo " "
+sleep 5s;
+
+sudo echo  > /etc/sysctl.conf
+
+sudo cat > /etc/sysctl.conf  << _FEOF
+net.ipv4.ip_forward = 0
+net.ipv4.conf.default.rp_filter = 1
+net.ipv4.conf.default.accept_source_route = 0
+kernel.sysrq = 0
+kernel.core_uses_pid = 1
+kernel.threads-max=65535
+kernel.msgmni = 16384
+kernel.msgmnb = 65535
+kernel.msgmax = 65535
+kernel.shmmax = 68719476736
+kernel.shmall = 4294967296
+kernel.shmmni = 4096
+kernel.sem = 5010 641280 5010 128
+net.ipv4.tcp_max_tw_buckets = 6000000
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_rmem = 4096 87380 16777216
+net.ipv4.tcp_wmem = 4096 65536 16777216
+net.core.wmem_default = 8388608
+net.core.rmem_default = 8388608
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+net.core.netdev_max_backlog = 200000
+net.ipv4.tcp_no_metrics_save = 1
+net.core.somaxconn = 65535
+net.core.optmem_max = 10000000
+net.ipv4.tcp_max_orphans = 32768
+net.ipv4.tcp_max_syn_backlog = 655360
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_synack_retries = 2
+net.ipv4.tcp_syn_retries = 2
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_mem = 94500000 915000000 927000000
+net.ipv4.tcp_fin_timeout = 10
+net.ipv4.tcp_keepalive_time = 300
+net.ipv4.tcp_keepalive_probes=10
+net.ipv4.tcp_keepalive_intvl=2
+net.ipv4.ip_local_port_range = 10000 65535
+net.ipv4.route.gc_timeout = 100
+net.ipv4.tcp_congestion_control=cubic
+net.ipv4.conf.lo.arp_ignore = 1
+net.ipv4.conf.lo.arp_announce = 2
+net.ipv4.conf.all.arp_ignore = 1
+net.ipv4.conf.all.arp_announce = 2
+fs.aio-max-nr = 1024000
+fs.file-max = 1024000
+kernel.pid_max=327680
+vm.swappiness = 0
+vm.max_map_count=655360
+
+_FEOF
+
+sudo sysctl -p
 
 
