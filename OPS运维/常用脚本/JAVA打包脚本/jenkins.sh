dfa53231e79adf492283566aeb3eb75ec13e7565
运维那些破事 <lizhao_jun@aliyun.com>
2019-02-07 10:17:22 +0800

    Create jenkins.sh

---
+++ OPS运维/常用脚本/JAVA打包脚本/jenkins.sh
@@ -0,0 +1,13 @@
+#jenkins步骤配置脚本
+
+source ~/.bash_profile
+
+### 环境信息 ##
+PROJECT_NAME=java_monitor   #JOB名称
+PACKAGE_NAME='java_monitor-*.jar'   #包名
+PROJECT_ROOT=/mapbar/app/project/$PROJECT_NAME
+FROM_DIR=/mapbar/app/soft/jenkins/workspace/$PROJECT_NAME/target/$PACKAGE_NAME   #这个地方可能会有所调整
+
+set +x
+## 使用脚本进行打包
+sh  /app/jenkins/scripts/build_jar.sh -n ${PROJECT_NAME} -r ${PROJECT_ROOT} -p ${PACKAGE_NAME} -f ${FROM_DIR}
